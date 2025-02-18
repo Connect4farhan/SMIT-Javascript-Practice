f4c7d3775cbf93c2d4b680dec15eb41500b720b0
connect4farhan <connect4farhanahmed@gmail.com>
2025-02-18 21:45:22 +0500

    precendence

---
+++ app.js
@@ -0,0 +1,42 @@
+console.log("operators in js");
+
+let a = 10;
+let b = 4;
+
+//------Addition-------
+console.log("a + B = ", a+b)
+
+//------Subtraction-------
+console.log("a - B = ", a-b)
+
+//------Division-------
+console.log("a / B = ", a/b)
+
+//------Multiplication-------
+console.log("a * B = ", a*b)
+
+//------Exponentiation-------
+console.log("a ** B = ", a**b)
+
+//------Remainder-------
+console.log("a % B = ", a%b)
+
+//------Addition-------
+console.log("++a = ", ++a)
+console.log("a++ = ", a++)  
+console.log("--a = ", --a)
+console.log("a-- = ", a--)
+console.log("a = ", a)
+console.log("a-- = ", a--)
+console.log("--a = ", a--)
+console.log("a = ", a)
+
+
+
+
+
+
+
+
+Multiplication, Division, Remainder
+// 4	+, -	Addition, Subtraction
