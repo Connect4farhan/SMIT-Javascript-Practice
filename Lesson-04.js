1c318951f1328dd72a00a440deaf7b65cbda4b82
connect4farhan <connect4farhanahmed@gmail.com>
2025-02-27 23:54:15 +0500

    lesson-04

--- Lesson-04.js
+++ Lesson-04.js
@@ -4,7 +4,65 @@
 //****** array ********
 //---------------------
 
-var marks =[80, 70, 40];
+var marks =[10, 20, 30, 40 ,50];
 
-console.log(marks);
-console.log(marks.lengh);
+
+// marks.unshift(10);
+// marks.shift();
+// marks.push(1000);
+// marks.pop(1000);
+// marks.splice(2, 0, "Lemon", "Kiwi" );
+// marks.slice(1);
+// marks.toString();
+
+
+//--(01)--//The length property returns the length (size) of an array:
+// console.log(marks.length); 
+
+//--(02)--//The typeof property returns the type of variable:
+// console.log(typeof marks);
+
+
+
+//--(03)--//The push() method adds a new element to an array (at the end):
+// marks.push(1000);
+// console.log(marks);
+
+
+
+//--(04)--//The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:
+// marks.unshift(1000);
+// console.log(marks);
+
+
+//--(05)--//The shift() method removes the first array element and "shifts" all other elements to a lower index.
+// marks.shift();
+// console.log(marks);
+
+
+//--(06)--//The pop() method removes the last element from an array:
+// marks.pop();
+// console.log(marks);
+
+//--(07)--//The JavaScript method toString() converts an array to a string of (comma separated) array values.
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.toString());
+
+
+//--(08)--//The splice() method can be used to add new items to an array:
+// marks.splice(2, 0, "Lemon", "Kiwi" );
+// console.log(marks);
+
+
+//--(09)--//The slice() method slices out a piece of an array into a new array:
+// marks.slice(1);
+// console.log(marks);
+
+
+
+
+
+
+
+
+
