0dcee62587511c5975e0d62be8f9d6d8508f083b
connect4farhan <connect4farhanahmed@gmail.com>
2025-03-09 01:41:33 +0500

    lesson-04 compelete

--- Lesson-04.js
+++ Lesson-04.js
@@ -59,10 +59,79 @@ var marks =[10, 20, 30, 40 ,50];
 // console.log(marks);
 
 
+// //----------------****************~~~~~~~Loops~~~~~~~~****************--------------------
+
+
+// // *******~~~~for loops~~~~*********
+// debugger
+// for(var i = 1; i <=10; i++){
+//     console.log(`2 * ${i} = ${i*2}`);
+//     document.write(`2 x ${i} = ${i*2} <br/>`);
+// }
 
 
 
+// // debugger
+// // //first iteration
+// i = 0 // 0
+// 0 < 3 // true
+// 0
+
+// // //second iteration
+// i = 1 // 1
+// 1 < 3 // true
+// 1
+
+// // //third iteration
+// i = 2 // 2
+// 2 < 3 // true
+// 2
+
+// // //fourth iteration
+// i = 3 // 3
+// 3 < 3 // true
+// 3 
+
+// // //fifth iteration
+// i = 4 //4
+// 4 < 3 // fasle
+
+// console.log(i);
+
+
+// //--------------------------------------------------------------------------
+// // -------~~~~~**** post Increament / pre Increment ****~~~~~----------
+
+// var num = 5;
+// console.log(`num value is = ${num}`);
+// var result = num + 1;
+// console.log(`add 1 in num value is  = ${result}`);
+// var result = num++;
+// console.log(` apply 1++ post increment value is = ${result}`);
+// console.log(`value of num = ${num}`);
+
+// var result = ++num;
+// console.log(`apply ++1 pre increment value is  ${result}`);
+// console.log(`value of num = ${num}`);
+
+
+// // ------------------------------------------------------------------------------
+// // -------~~~~~**** Grade Sysytem ****~~~~~----------
 
 
 
+// var marks = prompt("Enter a Number");
 
+// if (marks >= 90 && marks <= 100){
+//     console.log("A Grade");
+// } else if (marks >=80 && marks <= 89){
+//     console.log("B Grade");
+// } else if (marks >=70 && marks <= 79){
+//     console.log("C Grade");
+// } else if (marks >=60 && marks <=69){
+//     console.log("D Grade");
+// } else if (marks > 100){
+//     console.log("Invalid marks");
+// } else {
+//     console.log("Fail")
+// }
